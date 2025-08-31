9d3493009ce7e130a3a34c1ae8817cbabfeeab8b
Jorge Flores <jorgeafa.19@gmail.com>
2025-08-31 03:47:37 -0500

    Layout de Login y Splash, icono de la app

--- app/src/main/java/com/utp/wemake/WelcomeActivity.java
+++ app/src/main/java/com/utp/wemake/WelcomeActivity.java
@@ -1,6 +1,8 @@
 package com.utp.wemake;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,17 +10,33 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.google.android.material.button.MaterialButton;
+
 public class WelcomeActivity extends AppCompatActivity {
+
+    MaterialButton btnLogin, btnSignup;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_welcome);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        btnLogin = findViewById(R.id.btn_login);
+        btnSignup = findViewById(R.id.btn_create_account);
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(WelcomeActivity.this, LoginActivity.class));
+            }
+        });
+
+        btnSignup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(WelcomeActivity.this, SignupActivity.class));
+            }
         });
     }
 }
