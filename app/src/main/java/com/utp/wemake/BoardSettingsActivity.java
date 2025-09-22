52809fa8471b561c599f1a7515d01006e5f572b6
Jorge Flores <jorgeafa.19@gmail.com>
2025-09-22 02:39:01 -0500

    Diseño del layout de configuración

---
+++ app/src/main/java/com/utp/wemake/BoardSettingsActivity.java
@@ -0,0 +1,20 @@
+package com.utp.wemake;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class BoardSettingsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_board_settings);
+
+    }
+}
